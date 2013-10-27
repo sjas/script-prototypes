328081210cb9de3325453589400a342ea4dda795
sjas <teilzeittresor@web.de>
2013-10-27 18:09:31 +0100

    add readme and java xml creator sample

---
+++ java-XML-without-objects.java
@@ -0,0 +1,33 @@
+public String createXml(List<MyLocation> locationList) {
+    XmlSerializer serializer = Xml.newSerializer();
+
+    StringWriter stringWriter = new StringWriter();
+
+    try {
+        serializer.setOutput(stringWriter);
+        serializer.startDocument("UTF-8", true);
+        serializer.startTag("", "training");
+        if (locationList.size() > 0) {
+            serializer.startTag("", "datetime");
+            serializer.text(String.valueOf(locationList.get(o)
+                        .getTimeAsLong()));
+            serializer.endTag("", "datetime");
+        }
+        for (MyLocation location : locationList) {
+            serializer.startTag("", "location");
+            serializer.startTag("", "longitude");
+            serializer.text(String.valueOf(location.getLongitude()));
+            serializer.endTag("", "longitude");
+            serializer.startTag("", "latitude");
+            serializer.text(String.valueOf(location.getLatitude()));
+            serializer.endTag("", "latitude");
+            serializer.endTag("", "location");
+        }
+        serializer.endTag("", "training");
+        serializer.endDocument();
+    } catch (IllegalArgumentException e) {
+    } catch (IllegalStateException e) {
+    } catch (IOException e) {
+    }
+    return stringWriter.toString();
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Code snippets of my own and collected throwaway scripts from around the web for small tasks.
