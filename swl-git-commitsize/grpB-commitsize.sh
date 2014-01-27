6d78597c579f9206c68f7ca28805778b04ed02c8
sjas <teilzeittresor@web.de>
2014-01-27 19:19:43 +0100

    backup weil wichtig

---
+++ swl-git-commitsize/grpB-commitsize.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# sum up all blob sizes for a given author 
+
+#declare -a names=("gabriel" "michael" "dmitri" "matthias" "037" "0422");
+#declare -p names;
+
+clear;
+
+names=`echo "alexander"; echo "matthias"; echo "swt-028112"; echo "swt-035806"; `
+
+#for i in ${names[*]}; do
+
+for line in $(echo $names);do
+    author="";
+    commitsize="";
+    lines="";
+    #author=${names[i]};
+    author=$line
+    echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    echo $author;
+    commitsize=`git log -i --author="${author}" --stat | grep "java" | grep "|" | awk -F "|" '{print $2}' | awk -F " " '{print $1}' |  awk '{ SUM += $1} END { print SUM }'`;
+    #echo $commitsize;
+    #lines=`git log --author="${author}" --stat  | grep "java" | grep "|" | wc -l`; 
+    lines=`git shortlog -ns | grep -i ${author} | awk -F " " '{ print $1 }'|  awk '{ SUM += $1} END { print SUM }'`
+    #echo $lines;
+    ergebnis=$(($commitsize/$lines)); 
+    echo  $ergebnis durchschnittliche aenderungen bei $lines commits... = $commitsize;
+done;
